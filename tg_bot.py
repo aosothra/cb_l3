0e4faac9a308b3cce458eee7ae37a902d4fad646
Govno <arhdam@yandex.ru>
2022-05-26 21:58:13 +0400

    setup dialogflow

--- tg_bot.py
+++ tg_bot.py
@@ -1,7 +1,37 @@
 from environs import Env
-
+from google.cloud import dialogflow
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+
+
+def detect_intent_texts(project_id, session_id, text, language_code):
+    """Returns the result of detect intent with texts as inputs.
+
+    Using the same `session_id` between requests allows continuation
+    of the conversation."""
+
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(project_id, session_id)
+    print("Session path: {}\n".format(session))
+
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+
+    print("=" * 20)
+    print("Query text: {}".format(response.query_result.query_text))
+    print(
+        "Detected intent: {} (confidence: {})\n".format(
+            response.query_result.intent.display_name,
+            response.query_result.intent_detection_confidence,
+        )
+    )
+    print("Fulfillment text: {}\n".format(response.query_result.fulfillment_text))
 
 
 def start(update: Update, context: CallbackContext):
@@ -9,7 +39,9 @@ def start(update: Update, context: CallbackContext):
 
 
 def echo(update: Update, context: CallbackContext):
-    update.message.reply_text(update.message.text)
+    text = update.message.text
+    detect_intent_texts('moonlit-grail-296914', update.effective_chat.id, text, 'ru-RU')
+    update.message.reply_text(text)
 
 
 def main():
