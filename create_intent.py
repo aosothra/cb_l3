a3f21d3895a625ab7120422303abd24a0ac05ab5
Govno <arhdam@yandex.ru>
2022-06-01 19:35:57 +0400

    create intents from standalone utility

--- create_intent.py
+++ create_intent.py
@@ -1,42 +1,19 @@
 import json
+from argparse import ArgumentParser
+
 from environs import Env
-from google.cloud import dialogflow
-from google.cloud.dialogflow import Intent
-
-
-def create_intent(project_id, display_name, training_phrases_parts, message_texts):
-    """Create an intent of the given intent type."""
-
-    intents_client = dialogflow.IntentsClient()
-
-    parent = dialogflow.AgentsClient.agent_path(project_id)
-
-    training_phrases = []
-    for training_phrases_part in training_phrases_parts:
-        part = Intent.TrainingPhrase.Part(text=training_phrases_part)
-        # Here we create a new training phrase for each provided part.
-        training_phrase = Intent.TrainingPhrase(parts=[part])
-        training_phrases.append(training_phrase)
-
-    text = Intent.Message.Text(text=[message_texts])
-    message = Intent.Message(text=text)
-    print(message)
-    intent = Intent(
-        display_name=display_name, training_phrases=training_phrases, messages=[message]
-    )
-
-    response = intents_client.create_intent(
-        request={"parent": parent, "intent": intent}
-    )
-
-    print("Intent created: {}".format(response))
+from dialogflow_wrap import create_intent
 
 
 def main():
     env = Env()
     env.read_env()
 
-    with open('questions.json', 'r', encoding='utf-8') as intents_file:
+    parser = ArgumentParser()
+    parser.add_argument('path_to_json', help='fullpath to JSON with intents to create')
+    args = parser.parse_args()
+
+    with open(args.path_to_json, 'r', encoding='utf-8') as intents_file:
         intents_to_add = json.load(intents_file)
     
     for name, content in intents_to_add.items():
--- dialogflow_wrap.py
+++ dialogflow_wrap.py
@@ -1,4 +1,8 @@
-from google.cloud.dialogflow import SessionsClient, TextInput, QueryInput
+from environs import Env
+from google.cloud.dialogflow import (
+    SessionsClient, TextInput, QueryInput, 
+    IntentsClient, AgentsClient, Intent
+)
 
 
 def detect_intent_texts(project_id, session_id, text, language_code):
@@ -17,4 +21,29 @@ def detect_intent_texts(project_id, session_id, text, language_code):
     return (
         response.query_result.fulfillment_text, 
         response.query_result.intent.is_fallback
+    )
+
+
+def create_intent(project_id, display_name, training_phrases_parts, message_texts):
+    '''Create an intent of the given intent type.'''
+
+    intents_client = IntentsClient()
+
+    parent = AgentsClient.agent_path(project_id)
+
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = Intent.TrainingPhrase.Part(text=training_phrases_part)
+        training_phrase = Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = Intent.Message.Text(text=[message_texts])
+    message = Intent.Message(text=text)
+
+    intent = Intent(
+        display_name=display_name, training_phrases=training_phrases, messages=[message]
+    )
+
+    response = intents_client.create_intent(
+        request={'parent': parent, 'intent': intent}
     )
