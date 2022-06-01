fe94d8a6441c516427498e12e67eaf311c672969
Govno <arhdam@yandex.ru>
2022-06-01 17:40:40 +0400

    echo incoming messages

--- vk_bot.py
+++ vk_bot.py
@@ -1,24 +1,30 @@
-import vk_api
-from environs import Env
+import random
+
+import vk_api as vk
 from vk_api.longpoll import VkLongPoll, VkEventType
+
+from environs import Env
+
+
+def echo(event, vk_api):
+    vk_api.messages.send(
+        user_id=event.user_id,
+        message=event.text,
+        random_id=random.randint(1,1000)
+    )
 
 
 def main():
     env = Env()
     env.read_env()
 
-    vk_session = vk_api.VkApi(token=env('VK_ACCESS_TOKEN'))
-
+    vk_session = vk.VkApi(token=env('VK_ACCESS_TOKEN'))
+    vk_api = vk_session.get_api()
+    print(vk_api.messages)
     longpoll = VkLongPoll(vk_session)
-
     for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW:
-            print('Новое сообщение:')
-            if event.to_me:
-                print('Для меня от: ', event.user_id)
-            else:
-                print('От меня для: ', event.user_id)
-            print('Текст:', event.text)
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            echo(event, vk_api)
 
 
 if __name__ == '__main__':
