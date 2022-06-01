3f9c917b8ad0172d4800ea658a1c1bd71e39c455
Govno <arhdam@yandex.ru>
2022-06-01 18:13:25 +0400

    proccess incoming messages with dialogflow

--- tg_bot.py
+++ tg_bot.py
@@ -2,28 +2,13 @@ import logging
 import os
 
 from environs import Env
-from google.cloud import dialogflow
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
+from dialogflow_wrap import detect_intent_texts
+
 
 log = logging.getLogger(__file__)
-
-
-def detect_intent_texts(project_id, session_id, text, language_code):
-    '''Returns the result of detect intent with text as input.'''
-
-    session_client = dialogflow.SessionsClient()
-    session = session_client.session_path(project_id, session_id)
-
-    text_input = dialogflow.TextInput(text=text, language_code=language_code)
-    query_input = dialogflow.QueryInput(text=text_input)
-
-    response = session_client.detect_intent(
-        request={'session': session, 'query_input': query_input}
-    )
-
-    return response.query_result.fulfillment_text
 
 
 def start(update: Update, context: CallbackContext):
@@ -32,10 +17,10 @@ def start(update: Update, context: CallbackContext):
 
 def echo(update: Update, context: CallbackContext):
     text = detect_intent_texts(
-            os.getenv('GOOGLE_PROJECT_ID'),
-            update.effective_chat.id,
-            update.message.text,
-            'ru-RU'
+            project_id=os.getenv('GOOGLE_PROJECT_ID'),
+            session_id=update.effective_chat.id,
+            text=update.message.text,
+            language_code='ru-RU'
             )
     
     update.message.reply_text(text)
--- vk_bot.py
+++ vk_bot.py
@@ -1,15 +1,23 @@
+import os
 import random
 
 import vk_api as vk
 from vk_api.longpoll import VkLongPoll, VkEventType
-
 from environs import Env
 
+from dialogflow_wrap import detect_intent_texts
 
 def echo(event, vk_api):
+    text = detect_intent_texts(
+        project_id=os.getenv('GOOGLE_PROJECT_ID'),
+        session_id=event.user_id,
+        text=event.text,
+        language_code='ru-RU'
+        )
+    
     vk_api.messages.send(
         user_id=event.user_id,
-        message=event.text,
+        message=text,
         random_id=random.randint(1,1000)
     )
 
@@ -20,7 +28,7 @@ def main():
 
     vk_session = vk.VkApi(token=env('VK_ACCESS_TOKEN'))
     vk_api = vk_session.get_api()
-    print(vk_api.messages)
+
     longpoll = VkLongPoll(vk_session)
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
