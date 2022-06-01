71a6a411121f5ea2abab6386a1db7b1a2ad5c190
Govno <arhdam@yandex.ru>
2022-06-01 11:43:15 +0400

    cleanup debug code

--- tg_bot.py
+++ tg_bot.py
@@ -1,61 +1,63 @@
+import logging
+import os
+
 from environs import Env
 from google.cloud import dialogflow
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
 
-def detect_intent_texts(project_id, session_id, text, language_code):
-    '''Returns the result of detect intent with texts as inputs.
+log = logging.getLogger(__file__)
 
-    Using the same `session_id` between requests allows continuation
-    of the conversation'''
+
+def detect_intent_texts(project_id, session_id, text, language_code):
+    '''Returns the result of detect intent with text as input.'''
 
     session_client = dialogflow.SessionsClient()
-
     session = session_client.session_path(project_id, session_id)
 
     text_input = dialogflow.TextInput(text=text, language_code=language_code)
-
     query_input = dialogflow.QueryInput(text=text_input)
 
     response = session_client.detect_intent(
-        request={"session": session, "query_input": query_input}
-    )
-
-    print("=" * 20)
-    print("Query text: {}".format(response.query_result.query_text))
-    print(
-        "Detected intent: {} (confidence: {})\n".format(
-            response.query_result.intent.display_name,
-            response.query_result.intent_detection_confidence,
-        )
+        request={'session': session, 'query_input': query_input}
     )
 
     return response.query_result.fulfillment_text
 
 
 def start(update: Update, context: CallbackContext):
-    update.message.reply_text('Здравствуйте! Повторяю все что слышу.')
+    update.message.reply_text('Здравствуйте, задавайте вопросы.')
 
 
 def echo(update: Update, context: CallbackContext):
     text = detect_intent_texts(
-            'moonlit-grail-296914',
+            os.getenv('GOOGLE_PROJECT_ID'),
             update.effective_chat.id,
             update.message.text,
-            'ru-RU')
+            'ru-RU'
+            )
     
     update.message.reply_text(text)
+
+
+def error_handler(updat: object, context: CallbackContext):
+    #TODO: Implement proper error logging
+    pass
 
 
 def main():
     env = Env()
     env.read_env()
 
+    logging.basicConfig(level=logging.WARNING)
+    log.setLevel(logging.ERROR)
+
     updater = Updater(env('TG_TOKEN'))
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler('start', start))
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dispatcher.add_error_handler(error_handler)
 
     updater.start_polling()
     updater.idle()
