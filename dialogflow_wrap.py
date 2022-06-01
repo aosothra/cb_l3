317fc43c5ba0a3a055743fa00d3ff08868f4e365
Govno <arhdam@yandex.ru>
2022-06-01 18:11:32 +0400

    create dialogflow api wrapper

---
+++ dialogflow_wrap.py
@@ -0,0 +1,17 @@
+from google.cloud.dialogflow import SessionsClient, TextInput, QueryInput
+
+
+def detect_intent_texts(project_id, session_id, text, language_code):
+    '''Returns the result of detect intent with text as input.'''
+
+    session_client = SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+
+    text_input = TextInput(text=text, language_code=language_code)
+    query_input = QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={'session': session, 'query_input': query_input}
+    )
+
+    return response.query_result.fulfillment_text
